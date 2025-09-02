a3fd3602eef8795493a9f669d3b627dbbdbab34a
devikasharma04 <121706968+devikasharma04@users.noreply.github.com>
2025-09-02 09:23:24 -0700

    Create piechart.js

---
+++ js/piechart.js
@@ -0,0 +1,21 @@
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+
+var data = google.visualization.arrayToDataTable([
+  ['Task', 'Sites'],
+  ['Landscaping', 24],
+  ['Cleaning', 20],
+  ['Gardening', 48],
+  ['Litter/Trash Pickup', 8],
+]);
+
+var options = {
+  title: 'Types of Volunteering'
+  // Although gardening is the most common type of voluteering happening on BP day, the Berkeley Project had all kinds of volunteering happening in Spring 2025.
+};
+
+var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+  chart.draw(data, options);
+}
