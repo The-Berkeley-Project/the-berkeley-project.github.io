437a8421604cceaa728dac870fa058e331178299
Tiger Shi <tigershi08@gmail.com>
2025-11-18 01:44:43 -0800

    remove workflow

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,10 @@
 import type { NextConfig } from "next";
 
-const repoName = "the-berkeley-project.github.io";
-const isProduction = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProduction ? `/${repoName}` : undefined,
-  assetPrefix: isProduction ? `/${repoName}/` : undefined,
   trailingSlash: true,
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "www.berkeleyproject.org",
-      },
-    ],
   },
 };
 
