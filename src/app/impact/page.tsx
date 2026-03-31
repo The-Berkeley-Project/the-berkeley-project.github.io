82f26f7860b8e2aae07462c42356093739ee5b06
cjchengscu <cjcheng@scu.edu>
2026-03-31 00:13:29 -0700

    fixed build

--- src/app/impact/page.tsx
+++ src/app/impact/page.tsx
@@ -36,8 +36,8 @@ export default function ImpactPage() {
   const options = {
     plugins: {
       legend: {
-        position: "right",
-      }
+        position: "top" as const,
+      },
     },
   };
 
